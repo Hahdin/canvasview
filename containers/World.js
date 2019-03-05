bb0f9084b59b2e77714fcc629f9b8c7c5eed7d18
Mark Foster <mark.foster@abracontrols.com>
2019-03-04 23:23:15 -0700

    typo fix

--- containers/World.js
+++ containers/World.js
@@ -4,7 +4,7 @@ export const World = ({ ...props }) =>  {
   let canvas = null
   useEffect(() => {//did mount
     if (canvas) return
-    canvas = navier.create()
+    canvas = WorldObject.create()
     canvas.initCanvas()
     canvas._fill('rgba(0,0,0, 1)', 0, 0)
     canvas.start()

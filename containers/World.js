264b17381c33e4593765ba80dabf430f6cbcd711
Mark Foster <mark.foster@abracontrols.com>
2019-03-04 23:20:33 -0700

    replace use of react component class with hooks

--- containers/World.js
+++ containers/World.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react'
-import { charles } from '../objects'
-import { WorldObject } from '../objects'
-export class World extends Component { 
-  constructor(props) {
-    super(props)
-    this.state = {
-      canvas: null
-    }
-  }
-  componentDidMount() {
-    //let canvas = charles.create()
-    let canvas = WorldObject.create()
-    
+import React, { useEffect } from 'react';
+import {  WorldObject } from '../objects'
+export const World = ({ ...props }) =>  {
+  let canvas = null
+  useEffect(() => {//did mount
+    if (canvas) return
+    canvas = navier.create()
     canvas.initCanvas()
     canvas._fill('rgba(0,0,0, 1)', 0, 0)
     canvas.start()
-    this.state.canvas = canvas
+    return () =>{//unmount
+      myCleanup()
+    }
+  }, []);
+
+  const myCleanup = () =>{
+    if (!canvas) return
+    canvas.cleanup()
+    canvas.stop()
   }
-  componentWillUnmount(){
-    console.log('unmount')
-    this.state.canvas.cleanup()
-    this.state.canvas.stop()
-  }
-  render() {
-    return (<div className='container ' />)
-  }
+  return (<div className='container ' />)
 }
-export default World
